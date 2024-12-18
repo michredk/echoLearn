a1c1f90ea9b6e5d3a69946518e3c7bd160154ea0
rm46627 <michredk@gmail.com>
2024-12-18 20:28:07 +0100

    project init

---
+++ echolearn/echolearnApp.swift
@@ -0,0 +1,17 @@
+//
+//  echolearnApp.swift
+//  echolearn
+//
+//  Created by Michał Redkwa on 18/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct echolearnApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ echolearnTests/echolearnTests.swift
@@ -0,0 +1,17 @@
+//
+//  echolearnTests.swift
+//  echolearnTests
+//
+//  Created by Michał Redkwa on 18/12/2024.
+//
+
+import Testing
+@testable import echolearn
+
+struct echolearnTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
